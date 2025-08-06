12f7b41c383d686d51394b18ca5da44a549b29a2
JobDon <jobie@commandcentre.it>
2025-08-06 19:38:43 +0200

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'standalone', // optimizes for production hosting
+}
+
+module.exports = nextConfig
