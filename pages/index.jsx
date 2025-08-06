6b42c3d7980f634b18bebcda854a711539fe8f6a
JobDon <jobie@commandcentre.it>
2025-08-06 22:26:50 +0200

    Create index.jsx

---
+++ pages/index.jsx
@@ -0,0 +1,7 @@
+// pages/index.jsx
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-3xl">Welcome to Command Centre IT</h1>
+    </div>
+  )
